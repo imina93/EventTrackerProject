6c2e2faa72011b4df56507ca18a4bbffd95c677c
Isaac Mina <isaacmina93@gmail.com>
2021-10-08 14:46:40 -0700

    Create Boot project, add JPA project dependency

---
+++ PokemonBreeder/src/main/java/com/skilldistillery/pokemonbreeder/PokemonBreederApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.pokemonbreeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonBreederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonBreederApplication.class, args);
+	}
+
+}
